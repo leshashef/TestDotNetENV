e4bc7f666cbf51dd83ee837e3be1c51878e5d8a0
leshashef <lesha-shevchenko2001@mail.ru>
2025-01-25 01:39:00 +0300

    Добавьте файлы проекта.

---
+++ TestDotNetENV/Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+DotNetEnv.Env.Load();
+
+var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DBCon");
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new Exception("Connection string not found. Ensure the .env file is correctly configured and placed in the root directory.");
+}
+//Add connection string to the applications configuration system
+Console.WriteLine(connectionString);
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
