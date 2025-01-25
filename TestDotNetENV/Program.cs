11c614dc63d5067c1ab01a86e55ab152ebe54008
leshashef <lesha-shevchenko2001@mail.ru>
2025-01-25 03:15:55 +0300

    new tests

--- TestDotNetENV/Program.cs
+++ TestDotNetENV/Program.cs
@@ -1,19 +1,30 @@
 //dotnet run --launch-profile https
 //dotnet run --launch-profile http
 
+using DotNetEnv.Configuration;
+
 var builder = WebApplication.CreateBuilder(args);
 
-var env = Environment.GetEnvironmentVariable("ENV");
-// Add services to the container.
-DotNetEnv.Env.Load($"{env}.env");
+var env = Environment.GetEnvironmentVariable("ENV"); // получаем данные из launchSettings.json
 
-var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DBCon");
+DotNetEnv.Env.Load($"{env}.env"); // добавляем в Environment данные из .env 
+
+var connectionString = Environment.GetEnvironmentVariable("ConnectionStrings__DBCon"); // получаем добавленные данные
 if (string.IsNullOrEmpty(connectionString))
 {
     throw new Exception("Connection string not found. Ensure the .env file is correctly configured and placed in the root directory.");
 }
-//Add connection string to the applications configuration system
 Console.WriteLine(connectionString);
+
+string getAppsetting = builder.Configuration.GetSection("LogFile").Get<string>(); // получаем данные из appsetting.json
+Console.WriteLine(getAppsetting);
+
+var test = Environment.GetEnvironmentVariable("TestData"); 
+Console.WriteLine(test);
+
+builder.Configuration.AddDotNetEnv($"{env}.env"); // добавляем в контейнер конфигурации .env 
+string trygetenvConf = builder.Configuration.GetSection("TestData").Get<string>(); // после добавления можем его получить 
+Console.WriteLine(trygetenvConf);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
